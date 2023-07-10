875852780faebbd821fcc791422f1339cf21cdcf
Thay Mendes <thay.gm@live.com>
2023-07-10 12:13:29 -0300

    web+header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./header.css";
+import Web from "./web/index";
+import Mobile from "./mobile/index";
+
 
 
 function Header() {
-return <div>CABEÇALHO</div>;
+    const [isOpen, setIsOpen] = useState(false);
+return (
+    <div className='header'>
+        <div className='logo'>Thaísa GM Portifolio</div>
+        <div className='menu'>
+        <div className='web-menu'>
+            <Web/>
+        </div>
+        <div className='mobile-menu'>
+            <div onClick ={()=> setIsOpen(!isOpen)}>
+                <i class="fi-rr-apps menu-icon"></i>
+            </div>
+        {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen}/>}
+        </div>
+        
+    </div>
+</div>
+);
 
 }
 
---
+++ src/components/header/mobile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Mobile() {
+    return <div>versão para smartphone</div>
+
+}
+
+
+export default Mobile;
